ee76b8e9306498b29f26790756f9081f47671bf4
Levi <levimogford@gmail.com>
2021-04-01 03:54:59 -0500

    misc
    i was gonna add the audio device selection to ... zz i forgot. I need to retrace my steps because i believe i didnt go through with it and was looking into maybe setting the system's default audio device...
    
    i should really just rewrite this mess of a program.

--- Eihwaz/src/control/AudioController.java
+++ Eihwaz/src/control/AudioController.java
@@ -13,15 +13,14 @@ public class AudioController {
 	public static volatile ArrayList<String> audioClipNames = new ArrayList<String>();
 	public static volatile ArrayList<Boolean> audioPlaying = new ArrayList<Boolean>();
 	
+	/*
+	 * via native Java MediaPlayer API; plays to default audio output device
+	 */
 	public static synchronized void play(int audioClipIndex, int loopCount, int startTime, int endTime, String audioDevice) {
+		if(audioDevice.length() > 3) { play(audioClipIndex, loopCount, startTime, endTime); return; }
 		if(audioPlaying.get(audioClipIndex) == true) return;
-		// TODO Auto-generated method stub
-		//audioClips.get(audioClipIndex).setMicrosecondPosition((long)startTime*1000000);
-		//loopCount++;
 		Orient2.println("||" + audioClipNames.get(audioClipIndex));
-		//if(audioClipNames.get(audioClipIndex).contains("ALREADYRUNNING")) return;
-		//else audioClips.get(audioClipIndex).setStatus(MediaPlayer.Status.PLAYING); 
-		//audioClips.get(audioClipIndex).getBufferProgressTime();
+
 		audioClips.get(audioClipIndex).seek(new Duration(startTime));//.setLoopPoints(startTime*1000000, endTime*1000000);
 		if(endTime != 0) audioClips.get(audioClipIndex).setStopTime(new Duration(endTime));
 		else endTime = (int) audioClips.get(audioClipIndex).getTotalDuration().toMillis();
@@ -29,23 +28,24 @@ public class AudioController {
 
         Orient2.println("Starting audioClipIndex " + audioClipIndex + " of duration " + (endTime-startTime));
 		
-        //audioClipNames.set(audioClipIndex, audioClipNames.get(audioClipIndex) + "ALREADYRUNNING");
-		//Orient2.println("||" + audioClipNames.get(audioClipIndex));
 		audioPlaying.set(audioClipIndex, true);
 		audioClips.get(audioClipIndex).seek(new Duration(0));
+		
 		audioClips.get(audioClipIndex).play();//.start();
 		try {
 			Thread.sleep(endTime-startTime);
-	        //audioClipNames.set(audioClipIndex, audioClipNames.get(audioClipIndex).substring(0, "ALREADYRUNNING".length()));
-			//Orient2.println("||" + audioClipNames.get(audioClipIndex));
 			audioPlaying.set(audioClipIndex, false);
 			Thread.currentThread().interrupt();
 		} catch (InterruptedException e) {
-	        //audioClipNames.set(audioClipIndex, audioClipNames.get(audioClipIndex).substring(0, "ALREADYRUNNING".length()));
-			//audioClips.get(audioClipIndex).seek(new Duration(0));
 			audioPlaying.set(audioClipIndex, false);
 			Thread.currentThread().interrupt();
 		}
 	}
 
+	/*
+	 * via VLC, to allow for selecting the audio output device
+	 * */
+	public static synchronized void play(int audioClipIndex, int loopCount, int startTime, int endTime) {
+		
+	}
 }
